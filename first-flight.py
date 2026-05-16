09ee7e676450f693357c79b5f038d05caebb54f1
Anushka Sharma <anush2007ka@gmail.com>
2026-05-16 12:11:50 +0530

    initialized basic movement

--- first-flight.py
+++ first-flight.py
@@ -5,21 +5,25 @@ drone = Drone()
 drone.connect()
 drone.take_off()
 
-drone.move_down(20)
-time.sleep(2)
-drone.move_up(30)
-time.sleep(2)
 
-drone.move_left(20)
-time.sleep(2)
-drone.move_right(30)
-time.sleep(2)
+drone.set_speed(50)
+# drone.move_down(20)
+# time.sleep(2)
+# drone.move_up(30)
+# time.sleep(2)
+
+# drone.move_left(20)
+# time.sleep(2)
+# drone.move_right(30)
+# time.sleep(2)
 
 
 drone.move_forward(80)
 time.sleep(2)
 drone.move_backward(50)
 time.sleep(2)
+
+drone.rotate(200)
 
 
 
